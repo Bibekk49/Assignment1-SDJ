37d4ddcaa3d81d408e2dd99c6d4cfab076cf393e
Bibekk49 <148820288+Bibekk49@users.noreply.github.com>
2024-03-06 15:33:06 +0100

    Revert "ff"
    This reverts commit a74f81e9c34553f3e6eaf0d4e81baf47a3cfc5b0.

--- src/RunVinylApp.java
+++ src/RunVinylApp.java
@@ -1,30 +1,9 @@
 import javafx.application.Application;
-import javafx.application.Platform;
 
-public class RunVinylApp {
-  public static void main(String[] args) {
-    // Launch the JavaFX application
+public class RunVinylApp
+{
+  public static void main(String[] args)
+  {
     Application.launch(VinylApp.class);
-
-    // Create and start a background thread
-    Runnable backgroundTask = () -> {
-      // Code to be executed in the background thread
-      while (true) {
-        // Simulate some background work
-        System.out.println("Background thread is running...");
-        try {
-          Thread.sleep(1000); // Sleep for 1 second
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-      }
-    };
-
-    Thread backgroundThread = new Thread(backgroundTask);
-    backgroundThread.setDaemon(true); // Set the thread as a daemon (will terminate when the main thread exits)
-    backgroundThread.start();
-
-    // The main thread continues to execute other tasks
-    System.out.println("Main thread continues...");
   }
 }
